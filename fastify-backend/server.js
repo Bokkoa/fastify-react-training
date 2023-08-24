bcf52972b39645155873cf39c8eff2633c51ef34
Felipe Cruz <krivozznova@gmail.com>
2023-08-24 00:05:45 -0600

    feat(endpoints): first post and get examples

--- fastify-backend/server.js
+++ fastify-backend/server.js
@@ -16,7 +16,10 @@ const server = builder({
 
 const start = async () => {
   try {
-    await server.listen(PORT);
+    await server.listen({
+      port: PORT, 
+      host: '0.0.0.0'
+    });
   } catch (error) {
     server.log.error(error);
     process.exit(1);
